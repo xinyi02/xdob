d9e3d4b2f32bef21f661754f45aeee152b581924
xinyi02 <80622099+xinyi02@users.noreply.github.com>
2021-08-28 13:14:36 +0800

    Update ua.go

--- models/ua.go
+++ models/ua.go
@@ -14,7 +14,7 @@ func initUserAgent() {
 	} else {
 		logs.Info("更新User-Agent")
 		var err error
-		ua, err = httplib.Get(GhProxy + "https://raw.githubusercontent.com/cdle/xdd/main/ua.txt").String()
+		ua, err = httplib.Get(GhProxy + "https://raw.githubusercontent.com/xinyi02/xiaodao/main/ua.txt").String()
 		if err != nil {
 			logs.Info("更新User-Agent失败")
 		}
